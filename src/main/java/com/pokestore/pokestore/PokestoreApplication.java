fe2f358e5b281991cb33c145f3322f3663653f07
Davide Carrión <77077581+d3iBiD23@users.noreply.github.com>
2025-03-31 18:33:32 +0200

    1st commit

---
+++ src/main/java/com/pokestore/pokestore/PokestoreApplication.java
@@ -0,0 +1,13 @@
+package com.pokestore.pokestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokestore/pokestore/PokestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokestore.pokestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
